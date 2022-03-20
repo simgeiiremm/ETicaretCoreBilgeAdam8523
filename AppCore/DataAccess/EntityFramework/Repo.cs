acb5768abaf99e65fa9e1b0b7bff1323407e3707
Simge Işık <simgeiirem@outlook.com>
2022-03-20 19:37:20 +0300

    20 Mart

---
+++ AppCore/DataAccess/EntityFramework/Repo.cs
@@ -0,0 +1,17 @@
+﻿using AppCore.DataAccess.EntityFramework.Bases;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCore.DataAccess.EntityFramework
+{
+    public class Repo<TEntity, TDbContext> : RepoBase<TEntity, TDbContext> where TEntity : class, new() where TDbContext : DbContext, new()
+    {
+        public Repo() : base()
+        {
+
+        }
+        public Repo(TDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
---
+++ DataAccess/Entities/Urun.cs
@@ -0,0 +1,20 @@
+﻿using AppCore.Records.Bases;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Entities
+{
+    public class Urun :RecordBase
+    {
+        [Required] //string için yapılır null olmasını engeller
+        [StringLength(200)]
+        public string Adi { get; set; }
+
+        [StringLength(500)]
+        public string Aciklamasi { get; set; }
+        public double BirimFiyati { get; set; }
+        public int StokMiktari { get; set; }
+        public DateTime? SonKullanmaTarihi { get; set; }
+        public int KategoriId { get; set; } 
+        public Kategori Kategori { get; set; } //1-M ilişkiden dolayı kategori kategori oluşturmak zorundayız
+    }
+}
